430a744c45c3b11b02f438849a4c0ee4acb9b863
Dragfy11 <dragfy_dark@hotmail.com>
2021-04-27 14:29:10 +0200

    exo

---
+++ exo.py
@@ -0,0 +1,17 @@
+
+# distance = int  (input("Entrez la distance parcourue en pieds "))
+# distance_miles = distance / 5280 # conversion en miles
+# time =  float   (input ("Entrer le temps écoulé en secondes"))
+# time_hours = time / 3600 # conversion en heures
+# vitesse = distance_miles / time_hours
+# print("La vitesse moyenne est :", vitesse,"miles/heure")
+
+
+# prenom=input("Saisir votre prénom: ")
+# print("Bonjour monsieur "+ prenom)
+
+# prenom=input("Saisir votre prénom : ")
+# nom=input("Saisir votre nom : ")
+# annee_naissance=input("Saisir votre année de naissance: ")
+# age=2020-int(annee_naissance)
+# print("Bonjour monsieur "+ prenom+" "+nom+" votre âge est : "+str(age))
